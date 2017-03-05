3518798a160fb90f63f0f59615154ddc23f83baf
Gennady <gennady.lebedev@gmail.com>
2017-03-05 22:18:02 +0400

    fix data.sql bug - old data wasn't removed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+TRUNCATE TABLE publishers;
 INSERT INTO publishers (id, title) VALUES (1, 'Manning Publications Co.');
 INSERT INTO publishers (id, title) VALUES (2, 'Packt Publishing Ltd.');
 INSERT INTO publishers (id, title) VALUES (3, 'O’Reilly Media, Inc.');
+INSERT INTO publishers (id, title) VALUES (4, 'The Pragmatic Programmers, LLC.');
+INSERT INTO publishers (id, title) VALUES (5, 'Prentice Hall');
+COMMIT;
