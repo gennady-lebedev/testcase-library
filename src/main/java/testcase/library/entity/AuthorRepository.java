0bf6d726c9c335fd4b50841c45734532c9c0b31d
Gennady <gennady.lebedev@gmail.com>
2017-04-25 00:04:32 +0400

    add RepositoryRestResource annotation

--- src/main/java/testcase/library/entity/AuthorRepository.java
+++ src/main/java/testcase/library/entity/AuthorRepository.java
@@ -1,6 +1,8 @@
 package testcase.library.entity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/testcase/library/entity/BookRepository.java
+++ src/main/java/testcase/library/entity/BookRepository.java
@@ -1,6 +1,8 @@
 package testcase.library.entity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
