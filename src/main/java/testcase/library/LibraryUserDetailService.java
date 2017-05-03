c5aa431f719242e71740d02d1c858f1ab81f03ad
Gennady <gennady.lebedev@gmail.com>
2017-05-03 18:17:36 +0400

    add security config draft

---
+++ src/main/java/testcase/library/LibraryUserDetailService.java
@@ -0,0 +1,49 @@
+package testcase.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import testcase.library.entity.User;
+import testcase.library.entity.UserRepository;
+import testcase.library.entity.UserRoles;
+
+import java.util.List;
+
+@Component
+public class LibraryUserDetailService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public LibraryUserDetailService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = repository.findByName(username);
+        if(user == null)
+            throw new UsernameNotFoundException("User with name '" +
+                    username + "' does not exist");
+
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPassword(),
+                getAuthorities(user)
+        );
+    }
+
+    private List<GrantedAuthority> getAuthorities(User user) {
+        UserRoles role = user.getRole();
+        if(role == null)
+            role = UserRoles.READER;
+
+        return AuthorityUtils.createAuthorityList(role.getRoleName());
+    }
+}
---
+++ src/main/java/testcase/library/SecurityConfig.java
@@ -0,0 +1,48 @@
+package testcase.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/api/items/**").authenticated()
+                .and()
+            .httpBasic()
+                .realmName("The Great Library")
+                .and()
+            .csrf()
+                .disable();
+    }
+}
