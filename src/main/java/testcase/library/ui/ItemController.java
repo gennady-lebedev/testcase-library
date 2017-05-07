9ffa37a464307a9b23a3017593d8fcf8c9f294c7
Gennady <gennady.lebedev@gmail.com>
2017-05-07 17:06:05 +0400

    dummy items list page now shows items list

--- src/main/java/testcase/library/ui/ItemController.java
+++ src/main/java/testcase/library/ui/ItemController.java
@@ -1,11 +1,21 @@
 package testcase.library.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import testcase.library.entity.ItemRepository;
 
 @Controller
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
@@ -13,7 +23,8 @@ public class ItemController {
     }
 
     @RequestMapping(value = "/items", method = RequestMethod.GET)
-    public String getItems() {
+    public String getItems(Model model) {
+        model.addAttribute("items", itemRepository.findAll());
         return "items-list";
     }
 }
