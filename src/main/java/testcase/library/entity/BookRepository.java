0753803409ea6205ed74838fb45c76f9b22abcd7
Gennady <gennady.lebedev@gmail.com>
2017-05-09 10:56:16 +0400

    add draft books page

--- src/main/java/testcase/library/entity/BookRepository.java
+++ src/main/java/testcase/library/entity/BookRepository.java
@@ -1,8 +1,8 @@
 package testcase.library.entity;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/testcase/library/ui/ItemController.java
+++ src/main/java/testcase/library/ui/ItemController.java
@@ -1,20 +1,24 @@
 package testcase.library.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import testcase.library.entity.BookRepository;
 import testcase.library.entity.ItemRepository;
 
 @Controller
 public class ItemController {
 
     private final ItemRepository itemRepository;
+    private final BookRepository bookRepository;
 
     @Autowired
-    public ItemController(ItemRepository itemRepository) {
+    public ItemController(ItemRepository itemRepository, BookRepository bookRepository) {
         this.itemRepository = itemRepository;
+        this.bookRepository = bookRepository;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -26,5 +30,11 @@ public class ItemController {
     public String getItems(Model model) {
         model.addAttribute("items", itemRepository.findAll());
         return "items-list";
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public String getBooks(Model model, Pageable pageable) {
+        model.addAttribute("books", bookRepository.findAll(pageable));
+        return "books-list";
     }
 }
