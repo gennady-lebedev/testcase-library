948eacda756a76e515635287b8975e7aabd3840b
Gennady <gennady.lebedev@gmail.com>
2017-06-01 15:32:49 +0400

    add index page

--- src/main/java/testcase/library/ui/UserController.java
+++ src/main/java/testcase/library/ui/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,11 @@ public class UserController {
     public UserController(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
         this.encoder = encoder;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
